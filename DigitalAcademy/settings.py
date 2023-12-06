af76a156204dde08339bdc79854c2be0b60ca254
MrApelsin4ik <safonov.nikita.vv@gmail.com>
2023-12-06 23:41:21 +0300

    updated setting.py

--- DigitalAcademy/settings.py
+++ DigitalAcademy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_tiw8oli@f8)1@q*73b)h4!7r^@)k4y++3pu0lo&hgb2#1_8k)'
+    SECRET_KEY = 'django-insecure-_tiw8oli@f8)1@q*73b)h4!7r^@)k4y++3pu0lo&hgb2#1_8k)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
